574c409cae06adcf2a53e28e9c229c36adee6640
bongo-animal <nyl.julioduran@gmail.com>
2022-01-11 17:09:28 -0600

    actividad console

---
+++ ejercicio.js
@@ -0,0 +1,72 @@
+var x = false;
+var y = x.toString();
+var z = Number(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = true;
+var y = x.toString();
+var z = Number(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = 0;
+var y = x.toString();
+var z = Boolean(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = 1;
+var y = x.toString();
+var z = Boolean(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = "0";
+var y = Boolean();
+var z = Number(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = "000";
+var y = Boolean();
+var z = Number(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = "1";
+var y = Boolean();
+var z = Number(x);
+
+console.log(x);
+console.log(typeof(x));
+console.log(typeof(y));
+console.log(typeof(z));
+
+
+var x = NaN;
+va
